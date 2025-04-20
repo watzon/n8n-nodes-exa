4f9d0d13dad1f4f6db6a3dd24e094fccaf1fd06a
Chris Watson <cawatson1993@gmail.com>
2025-04-19 23:01:57 -0600

    take 45

--- .eslintrc.prepublish.js
+++ .eslintrc.prepublish.js
@@ -10,7 +10,6 @@ module.exports = {
 			plugins: ['eslint-plugin-n8n-nodes-base'],
 			rules: {
 				'n8n-nodes-base/community-package-json-name-still-default': 'error',
-				'n8n-nodes-base/node-param-collection-type-unsorted-items': 'off',
 			},
 		},
 	],
